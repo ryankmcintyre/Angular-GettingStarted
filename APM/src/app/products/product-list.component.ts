e15a11b25d710485ff7dffcac919d9b7aaccd36b
Ryan McIntyre <ryankmcintyre@gmail.com>
2021-03-08 11:08:13 -0700

    Completed module 10

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -12,6 +13,7 @@ export class ProductListComponent
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
     
     _listFilter: string;
     get listFilter(): string {
@@ -23,32 +25,13 @@ export class ProductListComponent
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2019",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-          },
-          {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          }        
-    ];
+    products: IProduct[] = [];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    constructor(private productService: ProductService) {
+    }
+    
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
     }
     
     performFilter(filterBy: string): IProduct[] {
@@ -62,6 +45,13 @@ export class ProductListComponent
     }
 
     ngOnInit(): void {
-        console.log('In OnInit');
+        this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
+        
     }
 }
